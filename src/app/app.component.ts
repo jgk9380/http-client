cff9ea1c726bf6990ff3a889878246e75de675a4
jgk9380 <jgk9380@sina.com>
2016-12-24 10:22:03 +0800

    add primeNG

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css','../../node_modules/primeng/resources/themes/omega/theme.css','../../node_modules/primeng/resources/primeng.min.css']
 })
 export class AppComponent {
+
+  clicks: number = 0;
+
+  count() {
+    this.clicks++;
+  }
   title = 'app work11123s!';
 }
